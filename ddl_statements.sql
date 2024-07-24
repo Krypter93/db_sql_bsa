bf85bdaf14d6959402b011e291b34a81c4ef78af
Ahmed <ahmedfdez93@gmail.com>
2024-07-23 23:46:30 -0400

    Working on DB and required tables

--- ddl_statements.sql
+++ ddl_statements.sql
@@ -1,2 +1,11 @@
-/* Create database */
-CREATE DATABASE MoviesDB;
+/* Create database */ CREATE DATABASE MoviesDB;
+
+/* Create Table User */
+CREATE TABLE User (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    Username VARCHAR(100) UNIQUE NOT NULL,
+    Firstname VARCHAR(100) NOT NULL,
+    Lastname VARCHAR(100) NOT NULL,
+    Email VARCHAR(50) UNIQUE NOT NULL, 
+    Password VARCHAR(100)
+    );
